cd022c66fd5bb475363295f8eefcd79960509e51
Sohan Dillikar <72825747+lil-zohee@users.noreply.github.com>
2021-01-03 15:10:12 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import alpaca_trade_api as tradeapi
 
-api_key = 'PK9OK29F28F1T3GN4QLK'
-secret_key = 'nrr1thKgXbpRgBWo43zxxwfakY6DZjWcjNEm4Szw'
+api_key = '' # <-- Your Alpaca API Key Here
+secret_key = '' # <-- Your Alpaca Secret Key Here
 api = tradeapi.REST(api_key, secret_key, 'https://paper-api.alpaca.markets')
